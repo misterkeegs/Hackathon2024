5fcf8465fcfded30efb8372052bce9e2d1e46c84
nol95 <dowlin1@stolaf.edu>
2024-02-24 13:30:31 -0600

    Multiple select dropdowns working

--- olesgo/src/App.js
+++ olesgo/src/App.js
@@ -1,6 +1,39 @@
 import React, { useState } from 'react';
+import Select from 'react-select';
+import makeAnimated from 'react-select/animated';
 import './App.css';
 import StOlafLogo from './images/StOlafPng.png';
+
+const days = [
+  { value: 'Monday', label: 'Monday' },
+  { value: 'Tuesday', label: 'Tuesday' },
+  { value: 'Wednesday', label: 'Wednesday' },
+  { value: 'Thursday', label: 'Thursday' },
+  { value: 'Friday', label: 'Friday' },
+  { value: 'Saturday', label: 'Saturday' },
+  { value: 'Sunday', label: 'Sunday' }
+];
+
+const purposes = [
+  { value: 'Medical', label: 'Medical' },
+  { value: 'Field Experience', label: 'Field Experience' },
+  { value: 'Work', label: 'Work' },
+  { value: 'Volunteering', label: 'Volunteering' },
+  { value: 'Other', label: 'Other' }
+];
+
+const animatedComponents = makeAnimated();
+
+const customStyles = {
+  control: (provided, state) => ({
+    ...provided,
+    width: '370px',
+    borderColor: state.isFocused ? '#263238' : '#263238',
+    '&:hover': {
+      borderColor: '#263238'
+    },
+  }),
+};
 
 function App() {
   const [selectedOption, setSelectedOption] = useState('option1');
@@ -30,7 +63,7 @@ function App() {
 
       </div>
         
-      <div>
+      <div  style={{marginLeft: '25px'}}>
       <form action="" id="loginForm">
         <h2><u>Personal Information</u></h2>
         <p></p>
@@ -97,14 +130,19 @@ function App() {
       )}
 
         <h3>Purpose</h3>
-        <select list="purpose" id="destination" class="required-field" required="true">
+        <Select 
+            components={animatedComponents}
+            options={purposes}
+            styles={customStyles}
+          />
+        {/*<select list="purpose" id="destination" class="required-field" required="true">
           <option value="none">(none)</option>
           <option value="Medical">Medical</option>
           <option value="Field Experience">Field Experience</option>
           <option value="Work">Work</option>
           <option value="Volunteering">Volunteering</option>
           <option value="Other">Other</option>
-        </select>
+      </select>*/}
         <p></p>
         <h3>Is this a Recurring Trip?</h3>
         <label>
@@ -119,13 +157,19 @@ function App() {
         {oneWayOption === 'option3' && (
         <div>
           <h3>Day(s) of the week</h3>
-        <input type="text" id="name" class="required-field" required="true" size="50"></input>
+          <Select 
+            closeMenuOnSelect={false}
+            components={animatedComponents}
+            options={days}
+            styles={customStyles}
+            isMulti
+          />
           <p></p>
         <h3>How often (weekly, bi-weekly, monthly)</h3>
         <input type="text" id="name" class="required-field" required="true" size="50"></input>
         <p></p>
         <h3>End date</h3>
-        <input type="text" id="name" class="required-field" required="true" size="50"></input>
+        <input type="date" id="pickup" name="pickup" required="true"></input>
         
         <p></p>
         
