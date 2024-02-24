7c4fd03dfe754e62a67307ddf958a2e532e613e5
nol95 <dowlin1@stolaf.edu>
2024-02-23 20:09:24 -0600

    Cleaned up default app

--- olesgo/src/App.js
+++ olesgo/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <h1>OlesGo Schedule Request</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://wp.stolaf.edu/transportation/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          St. Olaf Transportation Website
         </a>
-      </header>
     </div>
   );
 }
